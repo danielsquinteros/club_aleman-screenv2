8ce4da4bfe267f90be157a3abedce4a28fadb2a1
Nykko <n.perezbus@gmail.com>
2024-09-10 15:36:28 -0300

    fix: navmenu key error

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -6,114 +6,101 @@ const NavMenu = () => {
 
 	const menuItems = [
 		{
-			backgroundColor: "white",
-			
+			backgroundColor: 'white',
 		},
 		{
-			backgroundImage: "/images/home/bandera.png",
-			
+			backgroundImage: '/images/home/bandera.png',
 		},
 		{
-			backgroundColor: "white",
-			
+			backgroundColor: 'white',
 		},
 		{
-			backgroundColor: "black",
-			
+			backgroundColor: 'black',
 		},
 		{
-			backgroundColor: "black",
-			
+			backgroundColor: 'black',
 		},
 		{
-			backgroundColor: "white",
-			
+			backgroundColor: 'white',
 		},
 		{
-			backgroundImage: "/images/home/old_club.png",
-			
+			backgroundImage: '/images/home/old_club.png',
 		},
 		{
-			backgroundColor: "white",
-			
+			backgroundColor: 'white',
 		},
 		{
-			backgroundColor: "white",
+			backgroundColor: 'white',
 			icon: '/images/icons/circle_black.svg',
 			key: 'directors',
 			color: 'text-black',
 			to: '/directors',
 		},
 		{
-			backgroundColor: "black",
+			backgroundColor: 'black',
 			icon: '/images/icons/circle_white.svg',
 			key: 'members',
 			color: 'text-white',
 			to: '/members',
 		},
 		{
-			backgroundColor: "red",
+			backgroundColor: 'red',
 			icon: '/images/icons/circle_white.svg',
 			key: 'gallery',
 			color: 'text-white',
 			to: '/gallery',
 		},
 		{
-			backgroundColor: "orange",
+			backgroundColor: 'orange',
 			// icon: '/images/icons/circle_white.svg',
 			// key: 'history',
 			// color: 'text-white',
 			// to: '/history',
 		},
 		{
-			backgroundColor: "red",
-			
+			backgroundColor: 'red',
 		},
 		{
-			backgroundColor: "white",
-			
+			backgroundColor: 'white',
 		},
 		{
-			backgroundColor: "black",
-			
+			backgroundColor: 'black',
 		},
 		{
-			backgroundColor: "white",
-			
+			backgroundColor: 'white',
 		},
 	];
 
 	return (
 		<div className='grid grid-cols-4 grid-rows-4 w-full h-full'>
-
-			{menuItems.map(({ key, backgroundColor, backgroundImage, to, color, icon }) => (
-				<div 
-				style={{
-					backgroundColor,
-					backgroundImage: `url(${backgroundImage})`,
-					backgroundPosition: 'center',
-					backgroundRepeat: 'no-repeat',
-					objectFit: 'contain'
-				}}
-				className="w-full h-full flex items-center justify-center">
-					{
-						to && (
-								<div className='flex flex-col justify-center items-center'>
-									<Link to={to}>
-										<img
-											src={icon}
-											alt='Icono Círculo'
-											className='w-12 h-auto mb-4'
-										/>
-									</Link>
-									<p className={`text-2xl ${color}`}>
-										{t(key)}
-									</p>
-								</div>
-						)
-					}
-				</div>
-			))}
+			{menuItems.map(
+				({ key, backgroundColor, backgroundImage, to, color, icon }, index) => (
+					<div
+						key={index}
+						style={{
+							backgroundColor,
+							backgroundImage: `url(${backgroundImage})`,
+							backgroundPosition: 'center',
+							backgroundRepeat: 'no-repeat',
+							objectFit: 'contain',
+						}}
+						className='w-full h-full flex items-center justify-center'
+					>
+						{to && (
+							<div className='flex flex-col justify-center items-center'>
+								<Link to={to}>
+									<img
+										src={icon}
+										alt='Icono Círculo'
+										className='w-12 h-auto mb-4'
+									/>
+								</Link>
+								<p className={`text-2xl ${color}`}>{t(key)}</p>
+							</div>
+						)}
+					</div>
+				),
+			)}
 		</div>
 	);
 };
